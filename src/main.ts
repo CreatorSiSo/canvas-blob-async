df3cde51b33f39025f10852d8b2cdad364b75618
CreatorSiSo <64036709+CreatorSiSo@users.noreply.github.com>
2022-05-19 21:47:56 +0200

    Add qrcode generation and downloading via svg.

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,47 @@
 import "./style.css";
-import PDFDocument from "pdfkit";
+import QRCodeStyling from "qr-code-styling";
 
-const app = document.querySelector<HTMLDivElement>("#app")!;
+const labelLink = "https://co2unt.com/de/zertifikate/IX101-68049-56377";
 
-async function getBlobFromCanvas(
-	canvasElement: HTMLCanvasElement,
-	mime = "image/jpeg",
-	quality = 0.9
-) {
-	return new Promise<Blob>((resolve) => {
-		canvasElement.toBlob(
-			(blob) => (blob ? resolve(blob) : undefined),
-			mime,
-			quality
-		);
-	});
+const qrcode = new QRCodeStyling({
+	data: labelLink,
+	type: "svg",
+	width: 300,
+	height: 300,
+	image: "/drop.svg",
+	imageOptions: { hideBackgroundDots: true, margin: 8 },
+	dotsOptions: { type: "dots", color: "#034470" },
+	backgroundOptions: { color: "#ffffff00" },
+	cornersSquareOptions: { type: "extra-rounded", color: "#034470" },
+	cornersDotOptions: { type: "dot", color: "#034470" },
+	qrOptions: {
+		typeNumber: 0,
+		mode: "Byte",
+		errorCorrectionLevel: "H",
+	},
+});
+
+// Preview
+{
+	const previewElement = document.getElementById("preview") as HTMLDivElement;
+	qrcode.append(previewElement);
 }
 
-const canvas = document.querySelector("canvas")!;
-const canvasWidth = canvas.width;
-const ctx = canvas.getContext("2d");
-if (ctx) {
-	ctx.beginPath();
-	ctx.moveTo(canvasWidth / 2, 0);
-	ctx.lineTo(canvasWidth, canvasWidth);
-	ctx.lineTo(0, canvasWidth);
-	ctx.closePath();
-	ctx.fillStyle = "yellow";
-	ctx.fill();
+// Downloading
+{
+	const downloadSVGButton = document.getElementById(
+		"download-svg"
+	) as HTMLButtonElement;
+
+	downloadSVGButton.addEventListener("click", (_) =>
+		qrcode.download({ name: "IX101-68049-56377", extension: "svg" })
+	);
+
+	// const downloadPDFButton = document.getElementById(
+	// 	"download-pdf"
+	// ) as HTMLButtonElement;
+
+	// downloadPDFButton.addEventListener("click", (_) =>
+	// 	qrcode.download({ name: "IX101-68049-56377", extension: "svg" })
+	// );
 }
-
-const blob = await getBlobFromCanvas(canvas, "application/pdf");
-console.log(blob, URL.createObjectURL(blob));
-
-const blobAnchor = app.appendChild(document.createElement("a"));
-blobAnchor.textContent = URL.createObjectURL(blob);
-blobAnchor.href = URL.createObjectURL(blob);
-blobAnchor.target = "_blank";
-
-const doc = new PDFDocument();
-
-console.log(doc);
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,3 @@
 import { defineConfig } from "vite";
-import babel from "vite-plugin-babel";
 
-export default defineConfig({
-	plugins: [
-		babel({
-			babelConfig: {
-				babelrc: false,
-				configFile: false,
-				plugins: ["@babel/plugin-proposal-decorators"],
-			},
-		}),
-	],
-});
+export default defineConfig({});
